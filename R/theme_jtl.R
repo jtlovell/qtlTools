2710d1e2192621cde5d9dd007242967e73c2c1ca
jtlovell <johntlovell@gmail.com>
2016-10-19 12:26:11 -0500

    Updated ggplot themes

--- R/theme_jtl.R
+++ R/theme_jtl.R
@@ -1,7 +1,9 @@
-#' @title simple custom theme
+#' @aliases theme_jtl theme_jtlbar
+#' @title simple custom themes
 #'
 #' @description
-#' \code{theme_jtl}  a theme for ggplot similar to theme bw, but better (i think)
+#' \code{theme_jtl}  two themes for ggplot similar to theme bw, but better (i think). One is
+#' appropriate for most types of plots (theme_jtl). The other is better for barplots (theme_jtlbar)
 #' @export
 theme_jtl <- function()
 {
@@ -35,4 +37,44 @@ theme_jtl <- function()
 
       axis.ticks.length = unit(.15, "cm"),
       axis.ticks = element_line(size=.5, color="black"))
+}
+
+theme_jtlbar <- function()
+{
+  base_size = 10
+  base_family = ""
+  txt.8 <- element_text(size = 8,
+                        colour = "black",
+                        face = "plain")
+  txt.10 <- element_text(size = 10,
+                         colour = "black",
+                         face = "plain")
+  txt.12 <- element_text(size = 12,
+                         colour = "black",
+                         face = "plain")
+  axrange <- list(y = c(50, 90), x = c(2, 5))
+  theme_classic(base_size = base_size,
+                base_family = base_family) +
+    theme(legend.key = element_blank(),
+          strip.text = txt.10,
+
+          text = txt.8,
+          plot.title = txt.12,
+          axis.title = txt.10,
+          axis.text = txt.8,
+          legend.title = txt.10,
+          legend.text = txt.8,
+
+          strip.background = element_blank(),
+
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          panel.grid.major.y = element_line(color = "grey",   size = 0.5),
+
+          panel.background = element_blank(),
+          panel.border = element_blank(),
+
+          axis.ticks.length = unit(0.15, "cm"),
+          axis.ticks = element_line(size = 0.5,  color = "grey"))
 }
