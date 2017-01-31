24e0d1b1fc45712f2db407396fe389e8a2becb84
jtlovell <johntlovell@gmail.com>
2017-01-31 09:33:22 -0600

    added functions and documentation

---
+++ R/newLG.R
@@ -0,0 +1,54 @@
+#' @title Re-order markers and re-form linkage groups
+#'
+#' @description
+#' \code{newLG} Using a named list of markers (list element names = LG names),
+#' re-assign markers to linkage groups and re-orient markers. Most of the code is
+#' taken from qtl::formLinkageGroups.#'
+#' Note: all intermediate calculations are dropped. All chromosomes are set as
+#' autosomes.
+#'
+#' @param cross The QTL cross object.
+#' @param markerList A named list where element names are the chromosome IDs. Each
+#' element must contain a character vector specifying marker names. Markers in
+#' cross, but not in markerList will be dropped. Any markers in markerList that are not
+#' in markernames(cross) will be dropped.
+#'
+#' @details If any chromosomes are sex-specific, re-class such chromosomes after running
+#' newLG: using class(cross$geno[["Xchrom"]]) <- "S" or whatever. The new map has
+#' arbitrary cM positions where each marker is separated by 10cM. Run est.map to
+#' get true cM positions.
+#'
+#' @return  A cross object with markers reordered.
+#'
+#' @examples
+#' library(qtlTools)
+#' data(fake.bc)
+#' cross<-fake.bc
+#' \dontrun{
+#' ... more here ...
+#' }
+#' @import qtl
+#' @export
+newLG<-function(cross, markerlist){
+  markerlist<-markerlist[markerlist %in% markernames(cross)]
+  cross<-clean(cross)
+  n.mar<-nmar(cross)
+  crosstype <- class(cross)[1]
+  g <- pull.geno(cross)
+  cross$geno <- vector("list", length(markerlist))
+  names(cross$geno) <- names(markerlist)
+  for (i in names(markerlist)) {
+    mars<-markerlist[[i]]
+    cross$geno[[i]]$data <- g[, mars, drop = FALSE]
+    cross$geno[[i]]$map <- seq(0, by = 10, length = length(mars))
+    if (crosstype == "4way") {
+      cross$geno[[i]]$map <- rbind(cross$geno[[i]]$map,
+                                   cross$geno[[i]]$map)
+      colnames(cross$geno[[i]]$map) <- colnames(cross$geno[[i]]$data)
+    }else{
+      names(cross$geno[[i]]$map) <- colnames(cross$geno[[i]]$data)
+    }
+    class(cross$geno[[i]]) <- "A"
+  }
+  return(cross)
+}
---
+++ R/tspOrder.R
@@ -0,0 +1,85 @@
+#' @title Order markers within linkage groups
+#'
+#' @description
+#' \code{tspOrder} Apply a traveling salesperson problem solver to find the
+#' shortest path through the recombination fraction matrix. Requires that
+#' the TSP package is installed. Optimal performance is provided by the
+#' "Concorde" method; however, this requires independent installation of
+#' the Concorde program. See details.
+#'
+#' @param cross The QTL cross object.
+#' @param method The solve_TSP method to employ.
+#' @param hamiltonian Logical, should a hamiltonian circuit be enforced?
+#' @param concorde_path Required if method = "concorde". The directory containing
+#' concorde executables.
+#' @param return If "cross" is specified, pass the marker order through newLG, which quickly
+#' reorders the markers of the original cross to follow the TSP order. Otherwise,
+#' return a named list of markers in order for each chromosome.
+#' @param ... Additional arguments passed on to solve_TSP that are then paseed to TSP::contol.
+#' @details This function relies on the TSP R packages to perform the TSP solvers. See documentation
+#' therein, esspecially the function TSP::solve_TSP. We permit inference of marker order within a
+#' Hamiltonian circuit by adding a dummy node that has 0 distance to all other nodes. This
+#' allows for discrete start and end points and is more appropriate for genetic map construction
+#' than forcing a complete route through all markers.
+#'
+#' The best performance seems to result from using hamiltonian = T and method = "concorde".
+#'
+#' We recommend using the concorde algorithm. To do this, install the concorde program:
+#' http://www.math.uwaterloo.ca/tsp/concorde/downloads/codes/src/co031219.tgz.
+#' If on a mac, this is not super easy.
+#' See https://qmha.wordpress.com/2015/08/20/installing-concorde-on-mac-os-x/
+#' for details on the best way to do this.
+#'
+#' @return Either a cross object with reordered markers, or a named list
+#' of markers in a new order.
+#'
+#' @examples
+#' library(qtlTools)
+#' data(fake.bc)
+#' cross<-fake.bc
+#' \dontrun{
+#' ... more here ...
+#' }
+#' @import qtl
+#' @export
+tspOrder<-function(cross, markerList,
+                   method = "nn",
+                   hamiltonian = TRUE,
+                   concorde_path = NULL,
+                   return = "cross"){
+  if(!requireNamespace("TSP", quietly = TRUE)){
+    stop("install the TSP package to use tspOrder\n")
+  }
+  if(method == "concorde"){
+    if(is.null(concorde_path)){
+      stop("if method = concorde, concorde_path must specify directory of program\n")
+    }else{
+      concorde_path(concorde_path)
+    }
+  }
+  rf<-data.matrix(pull.rf(cross, what = "rf"))
+  class(rf)<-"matrix"
+  diag(rf)<-0
+
+  chr.ord<-lapply(names(markerList),function(x){
+    mnames<-markerList[[x]]
+    totsp<-rf[mnames,mnames]
+    chr.tsp<-TSP(totsp)
+    if(hamiltonian){
+      tsp <- insert_dummy(chr.tsp, label = "cut")
+      tour <- solve_TSP(tsp, method=method)
+      path <- cut_tour(tour, "cut")
+    }else{
+      path <- solve_TSP(chr.tsp, method=method)
+    }
+    ord<-labels(path)
+    return(ord)
+  })
+  names(chr.ord)<-names(markerList)
+  if(return != "cross"){
+    return(chr.ord)
+  }else{
+    out<-newLG(cross = cross, markerList = chr.ord)
+    return(out)
+  }
+}
