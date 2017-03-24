40e8f8e2646bbff3d227c1dd77114e1551de527f
jtlovell <johntlovell@gmail.com>
2017-03-23 22:54:32 -0500

    Initial

---
+++ R/pal_jtl.R
@@ -0,0 +1,3 @@
+pal_jtl<-c("black","darkred","cornflowerblue","gold",
+           "darkorchid3","lightseagreen","orangered","thistle","blue4","grey60",
+           "chartreuse","orange","turquoise1","darkolivegreen3","violet")
